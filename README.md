2befd435390eb3fed323c91b365882af441f3738
Jon Sykes <jon@sykes.me>
2012-11-13 16:29:23 -0500

    First pass at Ext.topic

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-extJS
-=====
+# Series of ExtJS extensions and components #
+
+
+## Ext.topic ##
+[https://github.com/jsykes/ext/topic](https://github.com/jsykes/ext/topic)
+
+Pub/sub topic system to allow anonymous communication within ExtJS.  Especially useful if you have components that are reused in various layouts/pages that need to either react to events or trigger events but don't want to be shackled by tightly coupled method calls.
+
+
+
+## License ##
+Copyright (c) 2011 Jon Sykes
+Dual licensed under the MIT and GPL licenses.
+
+Free as in Bacon.
---
+++ topic.js
@@ -0,0 +1,96 @@
+/*
+  ExtJs pub/sub plugin by Jon Sykes (jon@sykes.me)
+
+  Loosely based on Dojo's Pub/Sub Topic handlers
+  Code lifted heavily from the jquery implementation by Peter Higgins
+  https://github.com/phiggins42/bloody-jquery-plugins/blob/master/pubsub.js
+
+  Original Discussion: http://www.sencha.com/forum/showthread.php?144261
+
+*/
+var cache = {};
+
+Ext.define("Ext.topic", {
+  singleton: true,
+  // the topic/subscription hash
+  cache: {},
+
+  publish: function( /* String */ topic, /* Array? */ args) {
+    // summary:
+    //    Publish some data on a named topic.
+    // topic: String
+    //    The channel to publish on
+    // args: Array?
+    //    The data to publish. Each array item is converted into an ordered
+    //    arguments on the subscribed functions.
+    //
+    // example:
+    //    Add an event listener to a grid that publishes to the
+    //    something/happened topic on select
+    //
+    //  |   listeners: {
+    //  |     'select' : function(grid, rowIndex, e){
+    //  |       Ext.topic.publish("something/happened", [rowIndex]);
+    //  |     }
+    //  |   }
+    //
+    this.cache[topic] && Ext.each(this.cache[topic], function() {
+      //console.debug("Publish fires this function: ", this);
+      //this.apply(Ext.topic, args || []);
+      this.callback.apply(this.context || Ext.topic, args || []);
+    });
+  },
+
+  subscribe: function( /* String */ topic, /* Function */ callback, /* Object? */ context) {
+    //subscribe: function(/* String */topic, /* Function */callback){
+    // summary:
+    //    Register a callback on a named topic.
+    // topic: String
+    //    The channel to subscribe to
+    // callback: Function
+    //    The handler event. Anytime something is $.publish'ed on a
+    //    subscribed channel, the callback will be called with the
+    //    published array as ordered arguments.
+    //
+    // example:
+    //    Add an event listener to a panel that subscribes to the
+    //    something/happened topic and triggers it's handler function
+    //
+    // example:
+    //  |   listeners:{
+    //  |     render: function(){
+    //  |       Ext.topic.subscribe("something/happened", this.handleChangeWell);
+    //  |     }
+    //  |   }
+    //
+    if (!this.cache[topic]) {
+      this.cache[topic] = [];
+      //}
+      //this.cache[topic].push(callback);
+    }
+    this.cache[topic].push({
+      callback: callback,
+      context: context
+    });
+    return [topic, callback]; // Array
+  },
+
+  unsubscribe: function( /* Array */ handle) {
+    // summary:
+    //    Disconnect a subscribed function for a topic.
+    // handle: Array
+    //    The return value from a $.subscribe call.
+    // example:
+    //  |   var handle = Ext.topic.subscribe("something/happened", function(){});
+    //  |   Ext.topic.unsubscribe(handle);
+    //  |   // best practice would be to put these in an array and on destroy of
+    //  |   // the parent widget loop through and unsubsribe to all the topics.
+    var t = handle[0];
+    this.cache[t] && Ext.each(this.cache[t], function(name, index, itSelf) {
+      var name = name.callback;
+      if (name == handle[1]) {
+        this.cache[t].splice(index, 1);
+      }
+    }, this);
+  }
+});
